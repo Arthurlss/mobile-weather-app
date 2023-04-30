844a7edc8d3a04252fa2210f110500582901c5a1
Arthurlss <linnd.bberg@gmail.com>
2023-04-30 15:47:29 -0300

    montando_estrutura_base

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:my_weather/view/home_screen/widgets/header_home_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +10,30 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(gradient: getLinearGradient(true)),
+        child: Column(
+          children: [HeaderHomeScreen()],
+        ),
+      ),
+    );
+  }
+
+  dynamic getLinearGradient(check) {
+    if (check == true) {
+      return LinearGradient(colors: [
+        HexColor("#0e3881"),
+        HexColor("#124cb4"),
+        HexColor("#0d44ad"),
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+    } else {
+      return LinearGradient(colors: [
+        HexColor("#2eaedd"),
+        HexColor("#31a5d7"),
+        HexColor("#2ec7e9"),
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight);
+    }
   }
 }
