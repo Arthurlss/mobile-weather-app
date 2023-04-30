c25c90d78e488ce87fc04b5f39e13c5147ae3bc1
Arthurlss <linnd.bberg@gmail.com>
2023-04-30 18:28:11 -0300

    criando_componente_tempo_hoje

---
+++ lib/view/home_screen/widgets/today_weather_widget.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class TodayWeatherWidget extends StatefulWidget {
+  @override
+  State<TodayWeatherWidget> createState() => _TodayWeatherWidgetState();
+}
+
+class _TodayWeatherWidgetState extends State<TodayWeatherWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            width: double.infinity,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Today",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 25),
+                    ),
+                    Text(
+                      "Mar,9",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 25),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    getDayWeather(),
+                    getDayWeather(),
+                    getDayWeather(),
+                    getDayWeather(),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: HexColor("#0c3888"),
+                borderRadius: BorderRadius.circular(20)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getDayWeather() {
+    return Container(
+      width: 80,
+      child: Column(
+        children: [
+          Text(
+            "28°C",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25),
+          ),
+          SizedBox(
+            height: 22,
+          ),
+          Container(width: 60, child: Image.asset("assets/nuvem.png")),
+          SizedBox(
+            height: 22,
+          ),
+          Text(
+            "18:00",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25),
+          ),
+        ],
+      ),
+    );
+  }
+}
